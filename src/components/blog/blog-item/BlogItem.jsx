e49162cf04b8d88d6bdace4d939788192515640b
Olayinka247 <97066655+Olayinka247@users.noreply.github.com>
2022-05-17 08:48:44 +0100

    frontend

--- src/components/blog/blog-item/BlogItem.jsx
+++ src/components/blog/blog-item/BlogItem.jsx
@@ -1,22 +1,18 @@
 import React from "react";
 import { Card } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import BlogAuthor from "../blog-author/BlogAuthor";
+// import { Link } from "react-router-dom";
+// import BlogAuthor from "../blog-author/BlogAuthor";
 import "./styles.css";
 const BlogItem = (props) => {
-  const { title, cover, author, _id } = props;
+  const { title, cover, author } = props;
   return (
-    <Link to={`/blog/${_id}`} className="blog-link">
-      <Card className="blog-card">
-        <Card.Img variant="top" src={cover} className="blog-cover" />
-        <Card.Body>
-          <Card.Title>{title}</Card.Title>
-        </Card.Body>
-        <Card.Footer>
-          <BlogAuthor {...author} />
-        </Card.Footer>
-      </Card>
-    </Link>
+    <Card className="blog-card">
+      <Card.Img variant="top" src={cover} className="blog-cover" />
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+      </Card.Body>
+      <Card.Footer>${author}</Card.Footer>
+    </Card>
   );
 };
 
--- src/components/blog/blog-list/BlogList.jsx
+++ src/components/blog/blog-list/BlogList.jsx
@@ -1,22 +1,40 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import posts from "../../../data/posts.json";
 import BlogItem from "../blog-item/BlogItem";
+import { useState, useEffect } from "react";
 
-const BlogList = (props) => {
+const BlogList = () => {
+  const [blogPosts, setBlogPosts] = useState([]);
+
+  useEffect(() => {
+    getBlogs();
+  }, []);
+
+  const getBlogs = async () => {
+    let response = await fetch("http://localhost:3001/blogPosts");
+    if (response.ok) {
+      let data = response.json();
+      console.log(data);
+      setBlogPosts(data);
+    }
+  };
+
   return (
-    <Row>
-      {posts.map((post) => (
-        <Col
-          md={4}
-          style={{
-            marginBottom: 50,
-          }}
-        >
-          <BlogItem key={post.title} {...post} />
-        </Col>
-      ))}
-    </Row>
+    <>
+      <Row>
+        {blogPosts.map((blogPost) => (
+          <Col
+            md={4}
+            style={{
+              marginBottom: 50,
+            }}
+            key={blogPost.id}
+          >
+            <BlogItem blogPosts={blogPost} />
+          </Col>
+        ))}
+      </Row>
+    </>
   );
 };
 
